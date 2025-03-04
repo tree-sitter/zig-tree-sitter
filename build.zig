725484416653009d77632bb69c26d68a04b97b82
Niklas Koll <nkoll@databay.de>
2025-03-04 11:23:14 +0100

    Update to zig master/0.14

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const core = b.dependency("tree-sitter", .{
+    const core = b.dependency("tree_sitter", .{
         .target = target,
         .optimize = optimize,
     });
