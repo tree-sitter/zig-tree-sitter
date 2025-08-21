9069edd674b21bdc4a4a293d7fbbba339fd96392
ObserverOfTime <chronobserver@disroot.org>
2025-08-21 16:14:09 +0300

    fix(language): change `fieldIdForName` return type to `u16`

--- src/language.zig
+++ src/language.zig
@@ -127,7 +127,7 @@ pub const Language = opaque {
     }
 
     /// Get the numerical id for the given field name.
-    pub fn fieldIdForName(self: *const Language, field_name: []const u8) u32 {
+    pub fn fieldIdForName(self: *const Language, field_name: []const u8) u16 {
         return ts_language_field_id_for_name(self, field_name.ptr, @intCast(field_name.len));
     }
 
